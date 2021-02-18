ce84f6465865dc55a6811ecdb2691a778f075b19
NAEL VALIN <nael.valin@epita.fr>
2021-02-18 14:45:11 +0000

    Students

---
+++ source/repos/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public static List<Students> GetStudents()
+        {
+            List<Students> students = new List<Students>();
+            students.Add(new Students() { Id = 1, First_name = "Anton", Last_name = "Lafargue", Age = 19 });
+            students.Add(new Students() { Id = 1, First_name = "Marin", Last_name = "Bailhe", Age = 20 });
+            students.Add(new Students() { Id = 1, First_name = "Guilhem", Last_name = "Carlet", Age = 20 });
+            students.Add(new Students() { Id = 1, First_name = "Nael", Last_name = "Valin", Age = 19 });
+            students.Add(new Students() { Id = 1, First_name = "Julien", Last_name = "Matos", Age = 21 });
+            return students;
+        }
+        [HttpGet]
+        public IEnumerable<Students> GetStudents_List()
+        {
+            return GetStudents();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Students> GetStudents_ById(int id)
+        {
+            var students = GetStudents().Find(x => x.Id == id);
+            if (students != null)
+            {
+                return students;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
